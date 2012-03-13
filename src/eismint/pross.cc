61bbff526cceeebd0bf866ca9bfccc366b48b950
Matthias Mengel <mengel@pik-potsdam.de>
2012-03-13 15:48:16 +0100

    write netcdf fields after KSPSolver error.

--- src/eismint/pross.cc
+++ src/eismint/pross.cc
@@ -22,6 +22,7 @@
 #include "PISMIO.hh"
 #include "Timeseries.hh"
 #include "Mask.hh"
+#include "pism_signal.h"
 
 static char help[] =
   "Driver for EISMINT-Ross diagnostic velocity computation in ice shelf.\n"
