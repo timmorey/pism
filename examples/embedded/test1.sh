e33632561fd293577ce6ea06a3cdeb2ef82fb383
timmorey <timmorey@gmail.com>
2013-03-26 15:04:03 -0400

    don't use the skipping mechanism for now, since I don't really know how to come up with a good skip_max value.

--- examples/embedded/test1.sh
+++ examples/embedded/test1.sh
@@ -30,13 +30,13 @@ cp jakofine_short.nc jakofine-input.nc
 # and end of the time step
 # The parameters for this run are based on the first control run given in 
 # examples/searise-greenland/experiments.sh.
-#mpiexec -n 8 pismr \
-#  -bed_def lc -ssa_sliding -topg_to_phi 5.0,20.0,-300.0,700.0 -ocean_kill -climatic_mass_balance_cumulative \
-#  -ocean constant -atmosphere searise_greenland -surface pdd,turn_into_anomaly -pdd_annualize \
-#	-y 100 -step_count 1 \
-#	-i coarse-input.nc \
-#	-o coarse-output.nc \
-#	-step_record_file coarse-bc.nc -step_record_vars thk,usurf,bmelt,vel_ssa,enthalpy
+mpiexec -n 8 pismr \
+  -bed_def lc -ssa_sliding -topg_to_phi 5.0,20.0,-300.0,700.0 -ocean_kill -climatic_mass_balance_cumulative \
+  -ocean constant -atmosphere searise_greenland -surface pdd,turn_into_anomaly -pdd_annualize \
+  -y 100 -step_count 1 \
+  -i coarse-input.nc \
+  -o coarse-output.nc \
+  -step_record_file coarse-bc.nc -step_record_vars thk,usurf,bmelt,vel_ssa,enthalpy
 
 
 # Step 2: run pismo until it catches up to pismr
@@ -49,17 +49,13 @@ STOP_TIME=`python extract_time.py coarse-output.nc`
 # The parameters for this run are copied out of examples/jako/century.sh, with
 # only minor modifications to fit it into the embedded framework.
 mpiexec -n 8 pismo \
-	-i jakofine-input.nc \
-	-no_model_strip 10 \
-	-ocean_kill jako.nc -cfbc -kill_icebergs -diffuse_bwat -thk_eff -sia_e 1.0 -ssa_sliding -topg_to_phi 5.0,30.0,-300.0,700.0 -plastic_pwfrac 0.98 -pseudo_plastic -pseudo_plastic_q 0.25 \
-	-surface given,forcing -surface_given_file g5km_climate.nc -force_to_thk jako.nc \
-	-ys 0 -ye $STOP_TIME \
-	-skip -skip_max 100 \
-	-ssa_dirichlet_bc \
-	-coarse_grid_file coarse-bc.nc \
-	-o jakofine-output.nc \
-	-step_record_file jakofine-steps.nc -step_record_vars thk
-
+  -i jakofine-input.nc \
+  -no_model_strip 10 \
+  -ocean_kill jako.nc -cfbc -kill_icebergs -diffuse_bwat -thk_eff -sia_e 1.0 -ssa_sliding -topg_to_phi 5.0,30.0,-300.0,700.0 -plastic_pwfrac 0.98 -pseudo_plastic -pseudo_plastic_q 0.25 \
+  -surface given,forcing -surface_given_file g5km_climate.nc -force_to_thk jako.nc \
+  -ys 0 -ye $STOP_TIME \
+  -coarse_grid_file coarse-bc.nc \
+  -o jakofine-output.nc 
 
 # Step 3: combine pismr and pismo outputs to generate a new input for pismr
 
